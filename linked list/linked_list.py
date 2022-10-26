355f869b5882fc19e62d80a145069aac400c09e0
Yogeshwar Gadade <gadadeyogeshwar@gmail.com>
2022-10-26 18:29:59 +0530

    Updated Comments

--- linked list/linked_list.py
+++ linked list/linked_list.py
@@ -1,15 +1,19 @@
+# Node 
 class Node:
+    # Constructor
     def __init__(self,value):
         self.value = value
         self.next = None
 
+# Linked List
 class LinkedList:
+    # Constructor
     def __init__(self,value):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
         self.length = 1
-
+    # Append node to linkedlist
     def append(self,value):
         new_node = Node(value)
         if self.head is None:
@@ -20,7 +24,7 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
         return True
-
+    # Prepend node to linked list
     def prepend(self,value):
         new_node = Node(value)
         if self.head is None:
@@ -31,7 +35,7 @@ class LinkedList:
             self.head = new_node
         self.length += 1
         return True
-
+    # Insert node in linked list 
     def insert(self, value, index):
         if index < 0 or index > self.length:
             return None
@@ -46,8 +50,8 @@ class LinkedList:
         temp.next = new_node
         self.length += 1
         return True
-
-                
+    
+    # Pop last element of the lnked list            
     def pop(self):
         if self.head is None:
             #print('linked list is empty.')
@@ -65,7 +69,7 @@ class LinkedList:
                 self.head = None
                 self.tail = None
             return temp.value 
-
+    # Pop first element from linked list
     def pop_first(self):
         if self.head is None:
             return None
@@ -77,7 +81,7 @@ class LinkedList:
             if self.length == 0:
                 self.tail = None
             return temp.value
-
+    # Remove node from linked list
     def remove(self, index):
         if index < 0 or index > self.length-1:
             return None
@@ -92,13 +96,13 @@ class LinkedList:
         self.length -= 1
         return temp        
                      
-           
+    # Print Whole linked list       
     def print_list(self):
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
-
+    # Get perticuler node from linked list
     def get(self, index):
         if index < 0 or index >= self.length:
             return None
@@ -107,7 +111,7 @@ class LinkedList:
             for i in range(index):
                 temp = temp.next
             return temp
-
+    # Set perticuler node from linked list
     def set_value(self,index,value):
         temp = self.get(index)
         if temp:
@@ -115,7 +119,7 @@ class LinkedList:
              return True
         else:
             return False 
-
+    # Reverce whole linked list
     def reverse(self):
         temp = self.head
         self.head = self.tail
