f1e3db1c069ab8f56cb1f6622a14474093a56418
Yogeshwar Gadade <gadadeyogeshwar@gmail.com>
2022-10-29 11:17:04 +0530

    Updated Comments

--- Hash/hash.py
+++ Hash/hash.py
@@ -1,13 +1,16 @@
 class HashTable:
+    # constructor
     def __init__(self, size = 7):
         self.data_map = [None] * size
 
+    # Hash function
     def _hash(self, key):
         my_hash = 0
         for letter in key:
             my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
         return my_hash
 
+    # Print map
     def print_table(self):
         for i,val in enumerate(self.data_map):
             print(i, ": ", val) 
