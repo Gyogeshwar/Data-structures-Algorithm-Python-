48ca3450e69de40a7a08cdeb79a90e0e86dfb2cc
Yogeshwar Gadade <gadadeyogeshwar@gmail.com>
2022-11-04 10:06:33 +0530

    updated sort.py

--- Sort/Sort.py
+++ Sort/Sort.py
@@ -7,5 +7,18 @@ def BubbleSort(my_list):
                  my_list[j+1] = temp
     return my_list
 
-print(BubbleSort([5,3,2,9,8,1,3]))    
-                
+def SelectionSort(my_list):
+    for i in range(len(my_list)-1):
+        min_index = i
+        for j in range(i+1 , len(my_list)):
+            if  my_list[min_index]>my_list[j]:
+                min_index = j
+        if i != min_index:
+            temp = my_list[i]
+            my_list[i] = my_list[min_index]
+            my_list[min_index] = temp  
+    return my_list
+                   
+
+# print(BubbleSort([5,3,2,9,8,1,3]))    
+print(SelectionSort([5,3,2,9,8,1,3]))                    
